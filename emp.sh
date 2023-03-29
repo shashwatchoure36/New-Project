13d0569e73f0da387d81ec525e7a8d94ebd6b2b5
shashwatchoure365 <shashwatchoure36@gmail.com>
2023-03-29 12:15:49 +0530

    [Shashwat] Add : This script is used to calculating the employee wages

--- emp.sh
+++ emp.sh
@@ -1,14 +1,22 @@
-#!/bin/bash
+ #!/bin/bash
 
-echo "Welcome To employee computation program"
+ echo "Welcome To employee computation program"
 
-attendence_chk=$(( RANDOME%2 ))
-echo "$attendence_chk"
+ #Variable for checking the attendence
 
-if [ $attendence_chk -eq 1 ]
-then 
-    echo "Employee is Present"
+ attendence_chk=$((RANDOM%2)) #0/1
 
-else 
-    echo "Employee is Absent"
-fi
+ # Constant variable
+ is_present=1
+ full_day_hour=8
+ wage_per_hour=20
+ 
+ salary=0
+ # To check the attendence of the employee
+ if [ $attendence_chk -eq $is_present ]
+ then
+     salary=$(($full_day_hour * $wage_per_hour))
+     echo "Employee is Present for full day and the salary $salary"
+ else
+     echo "Employee is Absent and the salary is $salary"
+ fi
