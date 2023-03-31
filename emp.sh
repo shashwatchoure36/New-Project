26e03b72ab59c50d5fd60a7d7407b2fe172d2e1d
shashwatchoure365 <shashwatchoure36@gmail.com>
2023-03-31 17:46:58 +0530

    [Shashwat] Add : To Check the MOnthly salary of an Employee

--- emp.sh
+++ emp.sh
@@ -1,34 +1,51 @@
 
 #!/bin/bash
 
-echo "Welcome To Employee Wage Computation Program"
+echo "WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAM"
+echo "--------------------------------------------"
 
-# Variable for checking the attendence
-attendence_chk=$((RANDOM%3)) #0/1/2
-
-# constant variables
+#constant variables
 is_present_full_time=1
 is_present_part_time=2
 full_day_hour=8
-part_time_hour=4
+part_time=4
 wage_per_hour=20
+total_working_days=20
+max_working_hours=100
 
-
-# initializing the variables
+#initializing the variable
 salary=0
 daily_hour=0
+total_working_hour=0
+day=1
 
-case $attendence_chk in
+while [ $day -le $total_working_days -a $total_working_hour -le $max_working_hours ]
+do
+	attendance=$((RANDOM%3))		#Generates random number for attendance
 
-$is_present_full_time) daily_hour=8
-                       echo "The Employee is Present for Full day"
-                       ;;
-$is_present_part_time) daily_hour=4
-                       echo "The Employee is Present for Half day"
-                       ;;
-0) echo "The Employee is Absent"
-;;
-esac
+	case $attendance in 
+	$is_present_full_time) daily_hour=8
+						   echo "Employee is Present for Full time"
+					   		;;
+
+	$is_present_part_time) daily_hour=4
+					   	   echo "Employee is Present for Part time"
+					   		;;
+
+	0) daily_hour=0
+	   echo "Employee is Absent"
+   		;;
+	esac
+
 
 salary=$(($daily_hour * $wage_per_hour))
-echo "Salary of an Employee is $salary"
+echo "Salary of an Employee is : "$salary
+total_working_hour=$(($total_working_hour + $daily_hour))
+echo "Total working hours : "$total_working_hour
+echo "-----------------------------------"
+
+((day++))
+done
+
+monthly_salary=$(($total_working_hour * $wage_per_hour))
+echo "Monthly salary of an Employee is : "$monthly_salary
